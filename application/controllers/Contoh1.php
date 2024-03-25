471710e21895e31f1ee41142f787a18c0034be32
GrianDoyle <davincijr0@gmail.com>
2024-03-25 18:27:23 +0700

    Tugas1

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -6,6 +6,6 @@ class Contoh1 extends CI_Controller
         echo "<h1> Halo! Perkenalkan </h1>";
         echo "Nama saya Muhammad Raihan Ramadhan bisa di panggil Raihan atau Han
             Saya tinggal di Pontianak kota tepatnya di Tanjung Raya 2 
-            Saya sangat suka bermain game terutama game Competitif online";
+            Saya sangat suka bermain game terutama game Kompetitif online";
     }
 }  
