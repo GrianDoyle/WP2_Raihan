54e92e7c5c98cdb45a1a565eb2ebe7eb8ecabe64
GrianDoyle <davincijr0@gmail.com>
2024-03-25 17:26:59 +0700

    Tugas 1

---
+++ application/controllers/Tugas1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> Halo! Perkenalkan";
+        echo "Nama saya Muhammad Raihan Ramadhan bisa di panggil Raihan atau Han
+            Saya tinggal di Pontianak kota tepatnya di Tanjung Raya 2 
+            Saya sangat suka bermain game terutama game Competitif online";
+    }
+}  
