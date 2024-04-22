bd9b09828865e6dc410c2345315cf99c21d515a0
GrianDoyle <davincijr0@gmail.com>
2024-04-22 16:20:41 +0700

    Pertemuan 4 dan Tugas 3

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disk">
+        <li>Nama depan : Muhammad Raihan</li>
+        <li>Nama depan : Ramadhan</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="circle">
+        <li>Jalan Tanjung Harapan</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Pontianak</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulu Tangkis</li>
+    </ul>
+</section>
